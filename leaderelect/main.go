219438498e3da3dd6f65d3fd82d8bb05c3d61dbc
Yuriy Nasretdinov <nasretdinov@gmail.com>
2022-07-31 15:28:12 +0000

    Part 2 (final) of leader election implementation

--- leaderelect/main.go
+++ leaderelect/main.go
@@ -8,7 +8,9 @@ import (
 	"html/template"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 	"sync"
@@ -23,9 +25,10 @@ var indexHTML string
 var indexTPL = template.Must(template.New("index.html").Parse(indexHTML))
 
 type IndexData struct {
-	Leader      int
-	LeaderAlive bool
-	IsLeader    bool
+	Leader            int
+	LeaderAlive       bool
+	IsLeader          bool
+	IsElectionRunning bool
 
 	Messages []string
 }
@@ -36,9 +39,8 @@ var nodePortsArg = flag.String("nodePorts", "80,8081,8082", "List of nodes in th
 
 var nodePorts []int
 
-const ourGloriousLeader = 80
-
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	flag.Parse()
 
 	for _, p := range strings.Split(*nodePortsArg, ",") {
@@ -55,8 +57,8 @@ func main() {
 
 	if *server {
 		h := &webHandler{
-			leader:   ourGloriousLeader,
-			isLeader: (ourGloriousLeader == *port),
+			leader:   0,
+			isLeader: false,
 		}
 
 		go h.replicationLoop()
@@ -69,6 +71,8 @@ func main() {
 
 		// leader election endpoints
 		http.Handle("/leaderelection/leaderAlive", http.HandlerFunc(h.LeaderAlive))
+		http.Handle("/leaderelection/proposeLeader", http.HandlerFunc(h.ProposeLeader))
+		http.Handle("/leaderelection/finaliseLeader", http.HandlerFunc(h.FinaliseLeader))
 
 		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 	}
@@ -88,15 +92,41 @@ type webHandler struct {
 	leader      int
 	leaderAlive bool
 	isLeader    bool
+
+	isElectionRunning          bool
+	isLeaderPropositionRunning bool
+	electionCooldown           time.Time
 }
 
 func (h *webHandler) replicationLoop() {
 	for {
+		if h.getIsLeader() {
+			leaderPort, shouldTriggerReelection := h.getLeaderState()
+			if shouldTriggerReelection {
+				h.triggerReelection()
+			} else if leaderPort != 0 && leaderPort != *port {
+				log.Printf("Stepping down as a leader to majority leader %d", leaderPort)
+
+				h.Lock()
+				h.leader = leaderPort
+				h.isLeader = false
+				h.Unlock()
+			}
+		}
+
 		if err := h.replicationStep(); err != nil {
 			log.Printf("failed replication step: %v", err)
 			h.setLeaderAlive(false)
-			if h.shouldTriggerReelection() {
+			log.Printf("Talking about re-elections")
+			leaderPort, shouldTriggerReelection := h.getLeaderState()
+			if shouldTriggerReelection {
 				h.triggerReelection()
+			} else if leaderPort != 0 {
+				log.Printf("Changing leader to majority leader %d", leaderPort)
+
+				h.Lock()
+				h.leader = leaderPort
+				h.Unlock()
 			}
 		} else {
 			h.setLeaderAlive(true)
@@ -105,69 +135,234 @@ func (h *webHandler) replicationLoop() {
 	}
 }
 
-func (h *webHandler) shouldTriggerReelection() bool {
+func (h *webHandler) getLeaderState() (leaderPort int, shouldTrigger bool) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
-	log.Printf("Talking about re-elections")
-
 	type response struct {
-		alive bool
-		err   error
+		leaderPort int
+		alive      bool
+		err        error
 	}
 
 	respCh := make(chan response, len(nodePorts))
 
 	for _, p := range nodePorts {
 		go func(p int) {
-			alive, err := leaderAlive(ctx, p)
-			respCh <- response{alive: alive, err: err}
+			leaderPort, alive, err := leaderAlive(ctx, p)
+			respCh <- response{leaderPort: leaderPort, alive: alive, err: err}
 		}(p)
 	}
 
 	notAliveCnt := 0
-	minNotAlive := len(nodePorts)/2 + 1
+	majority := len(nodePorts)/2 + 1
+	leaderPorts := make(map[int]int)
 
 	for i := 0; i < len(nodePorts); i++ {
 		select {
 		case resp := <-respCh:
-			if !resp.alive && resp.err == nil {
-				notAliveCnt++
+			if resp.err == nil {
+				if !resp.alive {
+					notAliveCnt++
+				}
+
+				if resp.leaderPort != 0 {
+					leaderPorts[resp.leaderPort]++
+				}
 			}
+
 		case <-ctx.Done():
-			return false
+			return 0, false
 		}
 
-		if notAliveCnt >= minNotAlive {
-			return true
+		if notAliveCnt >= majority {
+			return 0, true
 		}
 	}
 
-	return false
+	for leaderPort, nodesCnt := range leaderPorts {
+		if nodesCnt >= majority {
+			return leaderPort, false
+		}
+	}
+
+	// there is no majority for the leader
+	return 0, true
 }
 
 func (h *webHandler) triggerReelection() {
-	log.Printf("REELECTION!!!")
+	h.setIsElectionRunning(true)
+	log.Printf("Starting election")
+
+	defer log.Printf("Election ended")
+	defer h.setIsElectionRunning(false)
+	defer h.setIsLeaderPropositionRunning(false)
+
+	firstSleep := true
+
+	for {
+		h.setIsLeaderPropositionRunning(false)
+
+		log.Printf("Doing random sleep")
+
+		if !firstSleep || firstSleep && !h.getIsLeader() {
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+		}
+
+		firstSleep = false
+
+		if !h.getIsElectionRunning() {
+			return
+		}
+
+		if time.Now().Before(h.getElectionCooldown()) {
+			log.Printf("Election is on cooldown")
+			continue
+		}
+
+		log.Printf("Proposing ourselves as a leader candidate")
+		h.setIsLeaderPropositionRunning(true)
+
+		num := proposeOurselvesAsLeaderCandidate()
+
+		if num >= len(nodePorts)/2+1 {
+			log.Printf("Finalising ourselves as a leader")
+			finaliseOurselvesAsLeader()
+			return
+		}
+	}
 }
 
-func leaderAlive(ctx context.Context, port int) (bool, error) {
-	url := fmt.Sprintf("http://localhost:%d/leaderelection/leaderAlive", port)
+func finaliseOurselvesAsLeader() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	var wg sync.WaitGroup
+
+	for _, p := range nodePorts {
+		wg.Add(1)
+		go func(p int) {
+			if err := finaliseOurselvesAsLeaderOnPort(ctx, p); err != nil {
+				log.Printf("Failed to finalise ourselves as leader on port %d: %v", p, err)
+			}
+			wg.Done()
+		}(p)
+	}
+
+	wg.Wait()
+}
+
+func proposeOurselvesAsLeaderCandidate() (successNodes int) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	errCh := make(chan error, len(nodePorts))
+
+	for _, p := range nodePorts {
+		go func(p int) {
+			err := proposeOurselvesAsLeaderOnPort(ctx, p)
+			if err == nil {
+				errCh <- nil
+			} else {
+				errCh <- fmt.Errorf("error proposing ourselves as a leader on node port %d: %v", p, err)
+			}
+		}(p)
+	}
+
+	for i := 0; i < len(nodePorts); i++ {
+		err := <-errCh
+		if err != nil {
+			log.Printf("Election error: %v", err)
+		} else {
+			successNodes++
+		}
+	}
+
+	return successNodes
+}
+
+func finaliseOurselvesAsLeaderOnPort(ctx context.Context, nodePort int) error {
+	u := url.Values{}
+	u.Set("port", fmt.Sprint(*port))
+
+	url := fmt.Sprintf("http://localhost:%d/leaderelection/finaliseLeader?"+u.Encode(), nodePort)
 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
-		return false, fmt.Errorf("making http request: %v", err)
+		return fmt.Errorf("making http request: %v", err)
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return false, fmt.Errorf("executing http request: %v", err)
+		return fmt.Errorf("executing http request: %v", err)
 	}
 
 	defer resp.Body.Close()
 
 	respBody, _ := ioutil.ReadAll(resp.Body)
 
-	return bytes.Equal(respBody, []byte("true")), nil
+	if bytes.Equal(respBody, []byte("true")) {
+		return nil
+	}
+
+	return fmt.Errorf("server responded with %s and status code %d", respBody, resp.StatusCode)
+}
+
+func proposeOurselvesAsLeaderOnPort(ctx context.Context, nodePort int) error {
+	u := url.Values{}
+	u.Set("port", fmt.Sprint(*port))
+
+	url := fmt.Sprintf("http://localhost:%d/leaderelection/proposeLeader?"+u.Encode(), nodePort)
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return fmt.Errorf("making http request: %v", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("executing http request: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	respBody, _ := ioutil.ReadAll(resp.Body)
+
+	if bytes.Equal(respBody, []byte("true")) {
+		return nil
+	}
+
+	return fmt.Errorf("server responded with %s and status code %d", respBody, resp.StatusCode)
+}
+
+func leaderAlive(ctx context.Context, port int) (int, bool, error) {
+	url := fmt.Sprintf("http://localhost:%d/leaderelection/leaderAlive", port)
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return 0, false, fmt.Errorf("making http request: %v", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return 0, false, fmt.Errorf("executing http request: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	respBody, _ := ioutil.ReadAll(resp.Body)
+
+	leaderPortStr, aliveStr, found := strings.Cut(string(respBody), "/")
+	if !found {
+		return 0, false, fmt.Errorf("response body: %s", respBody)
+	}
+
+	leaderPort, err := strconv.Atoi(leaderPortStr)
+	if err == nil {
+		return leaderPort, aliveStr == "true", nil
+	}
+
+	return 0, false, fmt.Errorf("response body: %s", respBody)
 }
 
 func (h *webHandler) setLeaderAlive(v bool) {
@@ -177,11 +372,46 @@ func (h *webHandler) setLeaderAlive(v bool) {
 	h.leaderAlive = v
 }
 
+func (h *webHandler) setIsElectionRunning(v bool) {
+	h.Lock()
+	defer h.Unlock()
+
+	h.isElectionRunning = v
+}
+
+func (h *webHandler) getIsElectionRunning() bool {
+	h.Lock()
+	defer h.Unlock()
+
+	return h.isElectionRunning
+}
+
+func (h *webHandler) setIsLeaderPropositionRunning(v bool) {
+	h.Lock()
+	defer h.Unlock()
+
+	h.isLeaderPropositionRunning = v
+}
+
+func (h *webHandler) getElectionCooldown() time.Time {
+	h.Lock()
+	defer h.Unlock()
+
+	return h.electionCooldown
+}
+
 func (h *webHandler) getLeaderPort() int {
 	h.Lock()
 	defer h.Unlock()
 
 	return h.leader
+}
+
+func (h *webHandler) getIsLeader() bool {
+	h.Lock()
+	defer h.Unlock()
+
+	return h.isLeader
 }
 
 func (h *webHandler) getStartIndex() int {
@@ -192,8 +422,12 @@ func (h *webHandler) getStartIndex() int {
 }
 
 func (h *webHandler) replicationStep() error {
-	if h.getLeaderPort() == *port {
+	leaderPort := h.getLeaderPort()
+
+	if leaderPort == *port {
 		return nil
+	} else if leaderPort == 0 {
+		return fmt.Errorf("need to elect a leader (port = 0)")
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
@@ -234,10 +468,11 @@ func (h *webHandler) List(w http.ResponseWriter, req *http.Request) {
 	defer h.Unlock()
 
 	indexTPL.Execute(w, &IndexData{
-		Leader:      h.leader,
-		IsLeader:    h.isLeader,
-		LeaderAlive: h.leaderAlive,
-		Messages:    h.data,
+		Leader:            h.leader,
+		IsLeader:          h.isLeader,
+		LeaderAlive:       h.leaderAlive,
+		Messages:          h.data,
+		IsElectionRunning: h.isElectionRunning,
 	})
 }
 
@@ -257,7 +492,7 @@ func (h *webHandler) LeaderAlive(w http.ResponseWriter, req *http.Request) {
 	h.Lock()
 	defer h.Unlock()
 
-	fmt.Fprintf(w, "%v", h.leaderAlive)
+	fmt.Fprintf(w, "%d/%v", h.leader, h.leaderAlive)
 }
 
 func (h *webHandler) Read(w http.ResponseWriter, req *http.Request) {
@@ -268,11 +503,6 @@ func (h *webHandler) Read(w http.ResponseWriter, req *http.Request) {
 	indexParam := req.Form.Get("index")
 	if indexParam == "" {
 		http.Error(w, "Index must not empty", http.StatusBadRequest)
-		return
-	}
-
-	if req.Form.Get("replicaPort") == "8081" {
-		http.Error(w, "You're not lucky", http.StatusBadRequest)
 		return
 	}
 
@@ -288,6 +518,75 @@ func (h *webHandler) Read(w http.ResponseWriter, req *http.Request) {
 	}
 
 	w.Write([]byte(strings.Join(h.data[index:], "\n")))
+}
+
+func (h *webHandler) ProposeLeader(w http.ResponseWriter, req *http.Request) {
+	h.Lock()
+	defer h.Unlock()
+
+	req.ParseForm()
+	portParam := req.Form.Get("port")
+	if portParam == "" {
+		http.Error(w, "Port must not empty", http.StatusBadRequest)
+		return
+	}
+
+	proposedPort, err := strconv.Atoi(portParam)
+	if err != nil || proposedPort < 0 {
+		http.Error(w, "Port must be a non-negative integer", http.StatusBadRequest)
+		return
+	}
+
+	if !h.isElectionRunning {
+		http.Error(w, "Currently not in leader election", http.StatusPreconditionFailed)
+		return
+	}
+
+	if h.isLeaderPropositionRunning && proposedPort != *port {
+		http.Error(w, "Currently proposing ourselves as a leader", http.StatusPreconditionFailed)
+		return
+	}
+
+	if proposedPort != *port {
+		h.electionCooldown = time.Now().Add(10 * time.Second)
+	}
+
+	w.Write([]byte("true"))
+}
+
+func (h *webHandler) FinaliseLeader(w http.ResponseWriter, req *http.Request) {
+	h.Lock()
+	defer h.Unlock()
+
+	req.ParseForm()
+	portParam := req.Form.Get("port")
+	if portParam == "" {
+		http.Error(w, "Port must not empty", http.StatusBadRequest)
+		return
+	}
+
+	proposedPort, err := strconv.Atoi(portParam)
+	if err != nil || proposedPort < 0 {
+		http.Error(w, "Port must be a non-negative integer", http.StatusBadRequest)
+		return
+	}
+
+	if !h.isElectionRunning {
+		http.Error(w, "Currently not in leader election", http.StatusPreconditionFailed)
+		return
+	}
+
+	if h.isLeaderPropositionRunning && proposedPort != *port {
+		http.Error(w, "Currently proposing ourselves as a leader", http.StatusPreconditionFailed)
+		return
+	}
+
+	h.isElectionRunning = false
+	h.leader = proposedPort
+	h.isLeader = (h.leader == *port)
+	h.leaderAlive = true
+
+	w.Write([]byte("true"))
 }
 
 func (h *webHandler) Append(w http.ResponseWriter, req *http.Request) {
